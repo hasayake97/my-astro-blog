0aba198a475a352ab5d4520eaf0a8be2c5469aa4
Rocky <rocky@nbi.ad>
2025-07-25 15:44:26 +0800

    ci: init repo

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import {defineConfig} from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+    adapter: netlify(),
+
+    integrations: [],
+
+    vite: {
+        plugins: [tailwindcss()]
+    }
+});
